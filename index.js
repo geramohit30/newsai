5482082f7232c7c9ba4a33baeb352dd173b8b7aa
Shivansh sharma <shivansh.sharma1@adani.com>
2025-03-18 22:57:59 +0530

    made for dummy api

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const connect = require('./Utils/mongo_utils');
+const fs = require('fs')
+const path = require('path')
 
 // Define a route
 app.get('/', (req, res) => {
@@ -9,11 +11,19 @@ app.get('/', (req, res) => {
 });
 app.get('/news',async (req,res)=>{
     try{
-        const db = await connect();
-        const col = db.collection('paras')
+        // const db = await connect();
+        // const col = db.collection('paras')
     
-        const data = await col.find({},{projection:{headline:1,articleBody:1,image:1,_id:0}}).toArray();
-        res.json({data:data})
+        // const data = await col.find({},{projection:{headline:1,articleBody:1,image:1,_id:0}}).toArray();
+        // res.json({data:data})
+        fs.readFile(path.join(__dirname,"parsed_data.json"),"utf-8",(err,data)=>{
+            if(err){
+                return res.json({"success":"False", "msg":"Failed to parse data!"})
+            }
+            const jsonData = JSON.parse(data);
+            const filteredData = jsonData.map(({headline,articleBody,image})=>({headline,articleBody,image}))
+            res.json(filteredData);
+        })
     }
     catch(error){
         console.log('Getting error', error);
