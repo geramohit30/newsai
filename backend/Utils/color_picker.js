5792a30ffe94cda1e026759959e255d05a647c2a
Neeraj Aggarwal <neeji@Neerajs-MacBook-Air.local>
2025-07-19 20:21:14 +0530

    added color gradient empty fix

--- backend/Utils/color_picker.js
+++ backend/Utils/color_picker.js
@@ -3,34 +3,34 @@ const Jimp = require('jimp');
 const ColorThief = require('colorthief');
 
 async function process_gradient(imageUrl) {
-    let gradient = [];
-    const toRGB = rgb => `rgb(${rgb.join(',')})`;
-    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const buffer = Buffer.from(response.data);
+  let gradient = [];
+  const toRGB = rgb => `rgb(${rgb.join(',')})`;
+  const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+  const buffer = Buffer.from(response.data);
 
-    let image = await Jimp.read(buffer);
-    image = image.resize(100, 100);
+  let image = await Jimp.read(buffer);
+  image = image.resize(100, 100);
 
-    const mime = image.getMIME();
-    const resizedBuffer = await image.getBufferAsync(mime);
+  const mime = image.getMIME();
+  const resizedBuffer = await image.getBufferAsync(mime);
 
-    const palette = await ColorThief.getPalette(resizedBuffer, 2);
-    if (!palette || palette.length < 2) {
-        rgb_flag = false;
-        console.warn(`Could not extract a valid color palette, using default colors. ${imageUrl}`);
-        gradient = [toRGB('#000000'), toRGB('#FFFFFF')];
-    }
-    else{
-        gradient = [toRGB(palette[0]), toRGB(palette[1])];
-        image = null;
-    }
-    global.gc && global.gc();
-    // If the palette extraction fails or returns less than 2 colors, use default colors
-    if (gradient.length < 2) {
-        console.warn(`no gradient found for the image, using default colors. ${imageUrl}`);
-        gradient = [toRGB('#000000'), toRGB('#FFFFFF')];
-    }
-    return gradient;
+  const palette = await ColorThief.getPalette(resizedBuffer, 2);
+  if (!palette || palette.length < 2) {
+    rgb_flag = false;
+    console.warn(`Could not extract a valid color palette, using default colors. ${imageUrl}`);
+    gradient = [toRGB([0, 0, 0]), toRGB([255, 255, 255])];
+  }
+  else {
+    gradient = [toRGB(palette[0]), toRGB(palette[1])];
+    image = null;
+  }
+  global.gc && global.gc();
+  // If the palette extraction fails or returns less than 2 colors, use default colors
+  if (gradient.length < 2) {
+    console.warn(`no gradient found for the image, using default colors. ${imageUrl}`);
+    gradient = [toRGB([0, 0, 0]), toRGB([255, 255, 255])];
+  }
+  return gradient;
 }
 
 async function getTwoColorGradient(imageUrl) {
@@ -43,7 +43,7 @@ async function getTwoColorGradient(imageUrl) {
   } catch (error) {
     console.error('Error generating gradient:', error.message);
     let toRGB = rgb => `rgb(${rgb.join(',')})`;
-    return [toRGB('#000000'), toRGB('#FFFFFF')];
+    return [toRGB([0, 0, 0]), toRGB([255, 255, 255])];
   }
 }
 
