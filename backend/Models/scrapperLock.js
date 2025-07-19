bc20cfea746c7f777adcc5524acbe4536350150e
Neeraj Aggarwal <neeji@Neerajs-MacBook-Air.local>
2025-07-19 20:41:53 +0530

    updated lock ttl to 1800 instead of 3600 sec

--- backend/Models/scrapperLock.js
+++ backend/Models/scrapperLock.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const scraperLockSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    createdAt: { type: Date, default: Date.now, index: { expires: 3600 } }
+    createdAt: { type: Date, default: Date.now, index: { expires: 1800 } }
 });
 
 module.exports = mongoose.model('ScraperLock', scraperLockSchema);
