db48045e61c76ea85c0ba417f8c72412abde7b75
Neeraj Aggarwal <neeji@Neerajs-MacBook-Air.local>
2025-07-20 11:37:35 +0530

    added lock release code

--- backend/Controllers/scrapingController.js
+++ backend/Controllers/scrapingController.js
@@ -10,22 +10,26 @@ const MongoConnect = require('../Utils/mongo_connect');
 
 exports.scrapeNow  = async (req, res) => {
   res.status(200).json({ message: 'Scraping is being processed...' })
+  const lockKey = 'scraper_lock';
   try {
     await MongoConnect();
     console.log('Manually triggering scraping...');
-    const lockKey = 'scraper_lock';
     try {
       await ScraperLock.create({ name: lockKey });
       console.log('Lock acquired. Running scraper.');
 
       await runScraping();
       await scrapeWebsite();
+      await ScraperLock.deleteOne({ name: lockKey }); // Release the lock after scraping
+      console.log('Lock released after scraping.');
       console.log('Scraping job completed!');
   } catch (error) {
       if (error.code === 11000) {
           console.log('Scraper already running or lock still valid. Skipping this run.');
           return
       } else {
+          await ScraperLock.deleteOne({ name: lockKey }); // Release the lock after scraping
+          console.log('releasing lock: scraping failure.');
           console.error('Error during scraping:', error);
           return
       }
