b3295220ff4f137258bb23d5c90d5525b3c3cb1a
Neeraj Aggarwal <neeji@Neerajs-MacBook-Air.local>
2025-07-23 15:38:40 +0530

    updated prompt

--- backend/Utils/chatgpt_utils.js
+++ backend/Utils/chatgpt_utils.js
@@ -8,11 +8,12 @@ const openai = new OpenAI({
 async function chatWithGPT4Mini(userPrompt, lang = 'en') {
 const englishPrompt = `Summarize the given English news article in less than 80 words. The summary should be crisp, up-to-date, and factually accurate, covering all the key elements of the story. Ensure the summary answers the 5Ws and 1H — What, Who, When, Where, Why, and How.
 Include any relevant data points, statistics, or factual figures, if mentioned.
+Make sure all punctuations are correct.
 The language must be clear, precise, and professional, as if written by an experienced news editor. Maintain a neutral and objective tone throughout. Avoid unnecessary adjectives, emotional framing, or commentary. 
 Text : ${userPrompt}`;
 const hindiPrompt = `Summarize the given Hindi news article in less than 80 words. The summary should be crisp, up-to-date, and factually accurate, covering all the key elements of the story. Ensure the summary answers the 5Ws and 1H — What, Who, When, Where, Why, and How.
 Include any relevant data points, statistics, or factual figures, if mentioned.
-The summary should be written in pure Hindi, with no use of English words, not even commonly used ones.
+Make sure all punctuations are correct.
 The language must be clear, precise, and professional, as if written by an experienced Hindi news editor. Maintain a neutral and objective tone throughout. Avoid unnecessary adjectives, emotional framing, or commentary. 
 Text : ${userPrompt}`;
   try {
